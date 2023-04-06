d78dcf63a72855ba385c337996a7c0bae8136843
Jordan Winslow <jordan@harmonize.health>
2023-04-06 14:17:40 -0400

    feat: add social auth with clerk and middleware

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,16 @@
-import { type AppType } from "next/app";
+import { type AppType } from "next/app"
 
-import { api } from "~/utils/api";
+import { api } from "~/utils/api"
 
-import "~/styles/globals.css";
+import { ClerkProvider } from "@clerk/nextjs"
+import "~/styles/globals.css"
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
-};
+  return (
+    <ClerkProvider {...pageProps}>
+      <Component {...pageProps} />
+    </ClerkProvider>
+  )
+}
 
-export default api.withTRPC(MyApp);
+export default api.withTRPC(MyApp)
