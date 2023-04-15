df3da9827fe1b802e2802254d310a68102302749
Jordan Winslow <jordan@harmonize.health>
2023-04-15 14:31:20 -0400

    chore: working on better auth experience

--- src/server/api/util/filterUserProperties.ts
+++ src/server/api/util/filterUserProperties.ts
@@ -3,9 +3,10 @@ import { type User } from "@clerk/nextjs/dist/api"
 export const filterUserProperties = (user: User) => {
   return {
     id: user.id,
-    userName: user.username || "randomuser " + user.id,
+    userName: user.username,
     firstName: user.firstName,
     lastName: user.lastName,
     image: user.profileImageUrl,
+    //externalUsername: user.externalAccounts.find((externalAccount) => externalAccount.provider === "oauth_github")?.username || null
   }
 }
